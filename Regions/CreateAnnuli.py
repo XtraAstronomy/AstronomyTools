aa8656fe583c3755cbaae95efb4461a86adc5b6e
Carter Rhea <carterrhea93@gmail.com>
2024-06-10 20:19:00 -0400

    Create annuli from X-ray image

--- Regions/CreateAnnuli.py
+++ Regions/CreateAnnuli.py
@@ -1,6 +1,8 @@
 """
 This code will create annuli of the same SNR given an X-ray image
 
+The X-ray image should **NOT** be exposure corrected!
+
 """
 import os
 from astropy.io import fits
@@ -9,19 +11,23 @@ import matplotlib.pyplot as plt
 from ciao_contrib.runtool import *
 
 
-def calculate_annuli(image, center, targetSNR, num_annuli, output_dir, pix2arcsec=0.492):
+def calculate_annuli(image_path, center, targetSNR, num_annuli, output_dir, pix2arcsec=0.492):
     """
 
     Args:
-        image:
-        center:
-        targetSNR:
-        num_annuli:
+        image_path: Path to image
+        center: Center of the annuli in image coordiantes
+        targetSNR: Target SNR
+        num_annuli: Number of annuli
+        output_dir: Output directory
+        pix2arcsec: Pixel size in arcsec
 
     Returns:
+        inner_radii: List of inner radii
+        outer_radii: List of outer radii
 
     """
-
+    image = fits.getdata(image_path).T  # Get image data
     radius_inner = 0  # Inner radius in pixels
     radius_outer = 1  # Outer radius in pixels
     inner_radii = []  # List of inner radii
@@ -31,7 +37,9 @@ def calculate_annuli(image, center, targetSNR, num_annuli, output_dir, pix2arcse
 
     annulus = 0  # Annulus number
     while annulus <= num_annuli:  # While the annulus number is less than the total number of desired annuli
-        sum_flux = np.sum(np.sum(image[cen_x+radius_inner:cen_x+radius_outer, cen_y+radius_inner:cen_y+radius_outer], axis=0), axis=0)  # Calculate SNR
+        sum_flux = np.sum(
+            np.sum(image[cen_x + radius_inner:cen_x + radius_outer, cen_y + radius_inner:cen_y + radius_outer], axis=0),
+            axis=0)  # Calculate SNR
         SNR = np.sqrt(sum_flux)
         if SNR > targetSNR:
             annulus += 1  # Update annulus number
@@ -40,19 +48,28 @@ def calculate_annuli(image, center, targetSNR, num_annuli, output_dir, pix2arcse
             radius_inner = radius_outer  # Update inner radius
             snr_list.append(SNR)  # Update SNR list
         radius_outer += 1  # Update outer annulus
-    plt.plot([val * pix2arcsec for val in outer_radii], snr_list, 'o')
+    x_values = [val * pix2arcsec for val in outer_radii]
+    plt.plot(x_values, snr_list, 'o')
     plt.xlabel('distance from center (arcsec)', fontsize=16, fontweight='bold')
     plt.ylabel('signal-to-noise', fontsize=16, fontweight='bold')
-    plt.savefig('%s/SNRvsDist.png'%output_dir)
+    plt.ylim(0, 1.25 * np.max(snr_list))
+    plt.xlim(x_values[0] - 5, x_values[-1] + 5)
+    plt.hlines(targetSNR, xmin=x_values[0] - 10, xmax=x_values[-1] + 10, label='SNR Target', linestyle='--', color='g')
+    plt.legend(loc='best')
+    plt.savefig('%s/SNRvsDist.png' % output_dir)
     return inner_radii, outer_radii
 
-def create_annuli_file(image_path, center, inner_radii, outer_radii, output_dir):
+
+def create_annuli_file(image_path, center, inner_radii, outer_radii, output_dir, pix2arcsec=0.492):
     """
 
     Args:
-        image:
-        inner_radii:
-        outer_radii:
+        image_path: Path to image
+        center: Center of the annuli in image coordiantes
+        inner_radii: List of inner radii
+        outer_radii: List of outer radii
+        output_dir: Output directory
+        pix2arcsec: Pixel size in arcsec
 
     Returns:
 
@@ -67,23 +84,20 @@ def create_annuli_file(image_path, center, inner_radii, outer_radii, output_dir)
     dmcoords()
     x, y = dmcoords.x, dmcoords.y
     annulus_ct = 0
-    for inner_radius,outer_radius in zip(inner_radii, outer_radii):
-        with open('%s/annulus_%i.reg'%(output_dir, annulus_ct), 'w+') as new_reg:
+    for inner_radius, outer_radius in zip(inner_radii, outer_radii):
+        with open('%s/annulus_%i.reg' % (output_dir, annulus_ct), 'w+') as new_reg:
             new_reg.write("# Region file format: DS9 version 4.1 \n")
             new_reg.write("physical \n")
-            new_reg.write('annulus(%s,%s,%.3f,%.3f)' % (x,y,inner_radius*0.492,outer_radius*0.492))
+            new_reg.write('annulus(%s,%s,%.3f,%.3f)' % (x, y, inner_radius * pix2arcsec, outer_radius * pix2arcsec))
         annulus_ct += 1
 
 
-image_path = '/home/crhea/Documents/Perseus/XrayFits/img_src.img'
-image = fits.getdata(image_path)
-image_hdr = fits.getheader(image_path)
+if __name__ == '__main__':
+    image_path = '/home/crhea/Documents/Perseus/XrayFits/img_src.img'
+    center = (215, 144)
+    output_dir = '/home/crhea/Documents/Perseus/XrayFits/regions'
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
-
-center = (215, 144)
-output_dir = '/home/crhea/Documents/Perseus/XrayFits/regions'
-
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-inner_radii, outer_radii = calculate_annuli(image.T, center, 35, 10, output_dir)
-create_annuli_file(image_path, center, inner_radii, outer_radii, output_dir)
+    inner_radii, outer_radii = calculate_annuli(image_path, center, 35, 10, output_dir)
+    create_annuli_file(image_path, center, inner_radii, outer_radii, output_dir)
